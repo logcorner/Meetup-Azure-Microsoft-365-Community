32f85fb11ff98f5882a9df13b8b7c6639ecf15d8
Gora LEYE <tocane.technologies@gmail.com>
2022-02-19 16:06:35 +0100

    clean up

--- workshop.TodoList/src/TodoList.SharedKernel.Repository/PaginationParameter.cs
+++ workshop.TodoList/src/TodoList.SharedKernel.Repository/PaginationParameter.cs
@@ -2,11 +2,10 @@
 {
     public class PaginationParameter
     {
-        //[Required]
-        //[Range(1, 50, ErrorMessage = "PageNumber should be number between 1 and 50.")]
+       
         public int PageNumber { get; set; }
 
-        // [Range(1, 20, ErrorMessage = "PageSize should be number between 1 and 20.")]
+        
         public int PageSize { get; set; }
     }
 }
